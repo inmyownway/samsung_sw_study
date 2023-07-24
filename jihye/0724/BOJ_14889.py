c425de7860594ed869f0092bf4e26e5973c4d43f
penguin1109 <1109penguin@gmail.com>
2023-07-25 01:58:44 +0900

    fix 14889

--- jihye/0724/BOJ_14889.py
+++ jihye/0724/BOJ_14889.py
@@ -2,6 +2,7 @@
 <문제 설명>
 - i번 사람과 j번 사람이 같은 팀에 속할때 팀에 S[ij]의 능력치가 더해진다.
 - 스타트 팀의 능력치와 링크 팀의 능력치의 차이를 최소로 만들고자 한다. -> 이렇게 될 수 있는 팀의 능력치의 차이의 최솟값을 구하여라.
+- 무조건 스타트팀과 링크 팀의 조원의 수는 N//2로 동일해야 한다.
 
 <풀이 방법>
 1. 우선 문제를 봤을 때는 그냥 백트래킹으로 모든 경우를 해보면 될 것도 같았다. 특히 4 <= N <= 20이기 때문에 해볼만 하다고 생각한다.
@@ -15,8 +16,9 @@ def find_min_diff(arr1, arr2, idx, sum1, sum2):
     global answer, S
     # print(arr1, arr2)
     if idx == N:
-        if abs(sum1 - sum2) < answer:
-            answer = abs(sum1 - sum2)
+        if len(arr1) == len(arr2):
+            if abs(sum1 - sum2) < answer:
+                answer = abs(sum1 - sum2)
         return
 
     # arr2.append(idx)
